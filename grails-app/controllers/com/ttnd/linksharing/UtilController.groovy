52ffa0d6f993f38d19eaad2a91d8b85110e80e77
Lovjit Bedi <lovjit.singh@tothenew.com>
2016-06-30 11:52:27 +0530

    Intro to Grails Commit

---
+++ grails-app/controllers/com/ttnd/linksharing/UtilController.groovy
@@ -0,0 +1,20 @@
+package com.ttnd.linksharing
+
+class UtilController {
+
+    def grailsApplication
+
+
+
+    def index() {
+
+        log.info("ola")
+        log.error("hola")
+        /*log.trace("trace")*/
+
+        /*def bar = grailsApplication.config.grails.project.class.dir
+        println(bar)*/
+
+
+    }
+}
